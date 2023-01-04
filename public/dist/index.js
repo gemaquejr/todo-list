eb12c68d7b71497e46e2852c9285f9be82b04755
gemaquejr <gemaquejr@hotmail.com>
2023-01-03 23:31:24 -0300

    Adiciona Enums e Functions

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,36 +1,67 @@
 "use strict";
 (() => {
+    let NotificationPlatform;
+    (function (NotificationPlatform) {
+        NotificationPlatform["SMS"] = "SMS";
+        NotificationPlatform["EMAIL"] = "EMAIL";
+        NotificationPlatform["PUSH_NOTIFICATION"] = "PUSH_NOTIFICATION";
+    })(NotificationPlatform || (NotificationPlatform = {}));
+    const UUID = () => {
+        return Math.random().toString(32).substring(2, 9);
+    };
+    const DateUtils = {
+        tomorrow() {
+            const tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            return tomorrow;
+        },
+        today() {
+            return new Date();
+        },
+        formatDate(date) {
+            return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+        }
+    };
     class Reminder {
         constructor(description, date, notifications) {
-            this.id = "";
-            this.dateCreated = new Date();
-            this.dateUpdated = new Date();
+            this.id = UUID();
+            this.dateCreated = DateUtils.today();
+            this.dateUpdated = DateUtils.today();
             this.description = "";
-            this.date = new Date();
-            this.notifications = ["EMAIL"];
+            this.date = DateUtils.tomorrow();
+            this.notifications = [NotificationPlatform.EMAIL];
             this.description = description;
             this.date = date;
             this.notifications = notifications;
         }
         render() {
-            return JSON.stringify(this);
+            return `
+            ---> Reminder <---
+            description: ${this.description}
+            date: ${DateUtils.formatDate(this.date)}
+            platform: ${this.notifications.join(",")}
+            `;
         }
     }
     class Todo {
         constructor(description) {
-            this.id = "";
-            this.dateCreated = new Date();
-            this.dateUpdated = new Date();
+            this.id = UUID();
+            this.dateCreated = DateUtils.today();
+            this.dateUpdated = DateUtils.today();
             this.description = "";
             this.done = false;
             this.description = description;
         }
         render() {
-            return JSON.stringify(this);
+            return `
+            ---> TODO <---
+            description: ${this.description}
+            done: ${this.done}
+            `;
         }
     }
     const todoMock = new Todo("Todo criado com a classe");
-    const reminderMock = new Reminder("Reminder criado com a classe", new Date(), ["EMAIL"]);
+    const reminderMock = new Reminder("Reminder criado com a classe", new Date(), [NotificationPlatform.EMAIL]);
     const taskView = {
         render(tasks) {
             const tasksList = document.getElementById("tasksList");
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
 (() => {
+    enum NotificationPlatform {
+        SMS = "SMS",
+        EMAIL = "EMAIL",
+        PUSH_NOTIFICATION = "PUSH_NOTIFICATION"
+    }
+
+    const UUID = (): string => {
+        return Math.random().toString(32).substring(2,9)
+    }
+
+    const DateUtils = {
+        tomorrow(): Date {
+            const tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            return tomorrow;
+        },
+
+        today(): Date {
+            return new Date()
+        },
+
+        formatDate(date: Date): string {
+            return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
+        }
+    }
+
     interface Task {
         id: string;
         dateCreated: Date;
@@ -8,29 +34,34 @@
     }
 
     class Reminder implements Task {
-        id: string = "";
-        dateCreated: Date = new Date();
-        dateUpdated: Date = new Date();
+        id: string = UUID();
+        dateCreated: Date = DateUtils.today();
+        dateUpdated: Date = DateUtils.today();
         description: string = "";
 
-        date: Date = new Date();
-        notifications: Array<string> = ["EMAIL"];
+        date: Date = DateUtils.tomorrow();
+        notifications: Array<NotificationPlatform> = [NotificationPlatform.EMAIL];
 
-        constructor(description: string, date: Date, notifications: Array<string>) {
+        constructor(description: string, date: Date, notifications: Array<NotificationPlatform>) {
             this.description = description;
             this.date = date;
             this.notifications = notifications;
         }
 
         render(): string {
-            return JSON.stringify(this);
+            return `
+            ---> Reminder <---
+            description: ${this.description}
+            date: ${DateUtils.formatDate(this.date)}
+            platform: ${this.notifications.join(",")}
+            `;
         }        
     }
 
     class Todo implements Task {
-        id: string = "";
-        dateCreated: Date = new Date();
-        dateUpdated: Date = new Date();
+        id: string = UUID();
+        dateCreated: Date = DateUtils.today();
+        dateUpdated: Date = DateUtils.today();
         description: string = "";
 
         done: boolean = false;
@@ -40,13 +71,17 @@
         }
 
         render(): string {
-            return JSON.stringify(this);
+            return `
+            ---> TODO <---
+            description: ${this.description}
+            done: ${this.done}
+            `;
         }        
     }
 
     const todoMock = new Todo("Todo criado com a classe")
 
-    const reminderMock = new Reminder("Reminder criado com a classe", new Date(), ["EMAIL"] )
+    const reminderMock = new Reminder("Reminder criado com a classe", new Date(), [NotificationPlatform.EMAIL] )
 
     const taskView = {
         render(tasks: Array<Task>) {
